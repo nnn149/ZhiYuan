03290fe1181c5bfb3cc622fca8d3a02d6e2d4673
Nannan <1041836312@qq.com>
2020-01-08 21:20:09 +0800

    头像设置

--- src/main/java/com/mno/controller/UserController.java
+++ src/main/java/com/mno/controller/UserController.java
@@ -37,7 +37,13 @@ public class UserController extends BaseServlet {
         User user = userService.getOneById(userId);
         Map<String, Object> info = new HashMap<>(4);
         info.put("introduction", user.getNickname());
-        info.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        if (user.getRole().equals("student")) {
+            info.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        } else if (user.getRole().equals("school")) {
+            info.put("avatar", "http://5b0988e595225.cdn.sohucs.com/q_70,c_zoom,w_640/images/20180928/478245be1df842f599ef3bfce76d734e.gif");
+        } else {
+            info.put("avatar", "http://pic1.zhimg.com/v2-c8a9b7c1f56cdee7c6a2b3303a23bfd8_b.gif");
+        }
         info.put("name", user.getUsername());
         List<String> list = new ArrayList<>();
         list.add(user.getRole());
